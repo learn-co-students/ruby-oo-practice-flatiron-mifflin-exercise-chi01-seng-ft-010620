676d3c9d9165f47b34ecbb6e63bba9519b7a6c87
github username <github email address>
2020-01-13 10:00:58 -0600

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,34 @@
 class Manager
+    attr_reader :name, :age, :department
 
+    @@all = []
+
+    def initialize(name, age, department)
+        @name = name 
+        @age = age
+        @department = department 
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def employees
+        Employee.all { |employees| employees.manager == self}
+    end 
+
+    def hire_employee(employee_name, salary)
+        Employee.new(employee_name, self, salary)
+    end 
+
+    def self.all_departments
+        @@all.map { |managers| managers.department}
+    end 
+
+    def self.average_age
+        ages = @@all.map { |managers| managers.age}
+        all_ages = ages.sum
+        all_ages / ages.count
+    end 
 end
--- run.rb
+++ run.rb
@@ -3,8 +3,21 @@ require_relative "lib/Employee"
 require 'pry'
 
 
-#Test your code here
+Mike = Manager.new("Mike", 45, "Chicago")
+Dave = Employee.new("Dave", "Mike", 35)
+Sara = Employee.new("Sara", "Mike", 57)
+Chris = Employee.new("Chris", "Sam", 52)
+Sam = Manager.new("Sam", 47, "NY")
+Ashley = Employee.new("Ashley", "Sam", 60)
+
+# Employee.all
+# Employee.paid_over(50)
+# p Employee.find_by_department("NY")
+# Manager.all_departments
+# p Manager.average_age
+p Chris.tax_bracket 
 
 
-binding.pry
+
+# binding.pry
 puts "done"
